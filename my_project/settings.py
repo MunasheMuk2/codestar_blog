1345f9eb92b10a1f27bf53c65b1ada18704ee91f
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-08-12 23:33:08 +0100

    Add a post model

--- my_project/settings.py
+++ my_project/settings.py
@@ -87,6 +87,7 @@ WSGI_APPLICATION = "my_project.wsgi.application"
 
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
+CSRF_TRUSTED_ORIGINS = ["https://*.codeinstitute-ide.net/", "https://*.herokuapp.com"]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators

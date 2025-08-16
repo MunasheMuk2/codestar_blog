4d4965e4d49c73e508c8ab0f7f34b76f0f7d2a7f
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-08-16 16:39:46 +0100

    generic post view

--- blog/admin.py
+++ blog/admin.py
@@ -3,3 +3,8 @@ from .models import Post
 
 # Register your models here.
 admin.site.register(Post)
+from django.contrib import admin
+from .models import Comment
+
+# Register your models here.
+admin.site.register(Comment)

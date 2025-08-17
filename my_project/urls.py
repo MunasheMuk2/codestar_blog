916137339117d4ac07ccdcc98006caf7afd0b613
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-08-17 01:19:26 +0100

    Enable serving pf static files

--- my_project/urls.py
+++ my_project/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for my_project project.
+URL configuration for codestar project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,7 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path("", include("blog.urls"), name="blog-urls"),
     path("admin/", admin.site.urls),
+    path("summernote/", include("django_summernote.urls")),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
